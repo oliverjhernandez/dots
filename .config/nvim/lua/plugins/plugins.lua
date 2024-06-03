b6032b9dec4cac3387b0c15bb168b461390a44b8
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-03 11:05:05 -0500

    feat: new colorscheme tokyonight

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,33 +1,20 @@
 return {
 
-  -- Terraform
-  "hashicorp/terraform-ls",
-  "hashivim/vim-terraform",
+  -- -- Terraform
+  -- "hashicorp/terraform-ls",
+  -- "hashivim/vim-terraform",
 
   -- colorschemes
-  { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "Verf/deepwhite.nvim", priority = 1000 },
   {
-    "scottmckendry/cyberdream.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        transparent = false,
-        italic_comments = true,
-        hide_fillchars = true,
-        borderless_telescope = true,
-        terminal_colors = false,
-        theme = {
-          variant = "light",
-        },
-      })
-    end,
+    opts = {},
   },
+  "rebelot/kanagawa.nvim",
 
   "taybart/b64.nvim",
-  "L3MON4D3/LuaSnip",
+  -- "L3MON4D3/LuaSnip",
 
-  "rcarriga/nvim-notify",
+  -- "rcarriga/nvim-notify",
 }
