eb36020bb01f3aa72b38b343210c4d116e63a12f
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-08 15:59:12 -0500

    chore: cleanup plugins

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,20 +1,9 @@
 return {
-
-  -- -- Terraform
-  -- "hashicorp/terraform-ls",
-  -- "hashivim/vim-terraform",
-
-  -- colorschemes
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
   },
-  "rebelot/kanagawa.nvim",
-
   "taybart/b64.nvim",
-  -- "L3MON4D3/LuaSnip",
-
-  -- "rcarriga/nvim-notify",
 }
