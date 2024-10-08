7e23e3e7db22f59c6c0bac4a9cd34bc8e3a22c00
oliverjhernandez <oliverjhernandez@gmail.com>
2024-10-08 18:22:10 -0500

    feat: refactor keymap variable

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,14 @@
-local keymap = vim.keymap
+local k = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "x", '"_x')
+k.set("n", "x", '"_x')
 
 -- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+k.set("n", "+", "<C-a>")
+k.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
+k.set("n", "dw", 'vb"_d')
 
 -- Select all
 -- keymap.set("n", "<C-a>", "gg<S-v>G")
@@ -17,79 +17,75 @@ keymap.set("n", "dw", 'vb"_d')
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+k.set("n", "<Leader>o", "o<Esc>^Da", opts)
+k.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+k.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
-keymap.set("n", "te", ":tabedit")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+k.set("n", "te", ":tabedit")
+k.set("n", "<tab>", ":tabnext<Return>", opts)
+k.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
--- Move window
--- keymap.set("n", "sh", "<C-w>h")
--- keymap.set("n", "sk", "<C-w>k")
--- keymap.set("n", "sj", "<C-w>j")
--- keymap.set("n", "sl", "<C-w>l")
+k.set("n", "ss", ":split<Return>", opts)
+k.set("n", "sv", ":vsplit<Return>", opts)
 
--- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+k.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
+k.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>")
+k.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>")
+k.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<CR>")
+k.set("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<CR>")
+k.set("n", "<C-Space>", "<cmd>NvimTmuxNavigateNext<CR>")
 
 -- Toggle Terminal
-keymap.set("n", "<C-\\>", ":ToggleTerm size=40 direction=float<cr>")
+k.set("n", "<C-\\>", ":ToggleTerm size=40 direction=float<cr>")
 -- keymap.set("n", "<leader>tv", ":ToggleTerm size=100 direction=vertical<cr>")
 -- keymap.set("n", "<leader>th", ":ToggleTerm size=100 direction=horizontal<cr>")
-keymap.set("t", "<C-\\>", "<C-\\><C-n><C-w>l")
+k.set("t", "<C-\\>", "<C-\\><C-n><C-w>l")
 
 -- Diagnostics
-keymap.set("n", "<leader>j", function()
+k.set("n", "<leader>j", function()
   vim.diagnostic.goto_next()
 end, opts)
 
-keymap.set("n", "<leader>k", function()
+k.set("n", "<leader>k", function()
   vim.diagnostic.goto_prev()
 end)
 
 -- Neogen
-keymap.set("n", "<leader>cc", function()
+k.set("n", "<leader>cc", function()
   require("neogen").generate({})
 end)
 
 -- Inc Rename
-vim.keymap.set("n", "<leader>rn", ":IncRename ")
+k.set("n", "<leader>rn", ":IncRename ")
 
 -- Kulala
-vim.keymap.set("n", "<leader>tr", function() -- Run test
+k.set("n", "<leader>tr", function() -- Run test
   require("kulala").run()
 end)
-vim.keymap.set("n", "<leader>ta", function()
+k.set("n", "<leader>ta", function()
   require("kulala")
 end)
-vim.keymap.set("n", "<leader>tt", function() -- Replay last test
+k.set("n", "<leader>tt", function() -- Replay last test
   require("kulala").replay()
 end)
-vim.keymap.set("n", "<leader>tc", function() -- Copy test as curl command
+k.set("n", "<leader>tc", function() -- Copy test as curl command
   require("kulala").run()
 end)
-vim.keymap.set("n", "<leader>tj", function() -- Go to next test
+k.set("n", "<leader>tj", function() -- Go to next test
   require("kulala").jump_next()
 end)
-vim.keymap.set("n", "<leader>tk", function() -- Go to previous test
+k.set("n", "<leader>tk", function() -- Go to previous test
   require("kulala").jump_prev()
 end)
 
 -- dial
-vim.keymap.set("n", "<C-a>", function()
+k.set("n", "<C-a>", function()
   require("dial.map").manipulate("increment", "normal")
 end)
-vim.keymap.set("n", "<C-x>", function()
+k.set("n", "<C-x>", function()
   require("dial.map").manipulate("decrement", "normal")
 end)
 
@@ -98,58 +94,58 @@ local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
 -- General search in the whole repo
-vim.keymap.set("n", "ff", function()
+k.set("n", "ff", function()
   builtin.find_files()
 end)
 
 -- "Searches for the string under your cursor or selection in your current working directory"
-vim.keymap.set("n", "fS", function()
+k.set("n", "fS", function()
   builtin.grep_string()
 end)
 
 -- "Lists available colorschemes and applies them on <cr>"
-vim.keymap.set("n", ";c", function()
+k.set("n", ";c", function()
   builtin.colorscheme()
 end)
 
 -- "Lists normal mode keymappings"
-vim.keymap.set("n", ";k", function()
+k.set("n", ";k", function()
   builtin.keymaps()
 end)
 
 -- "Lists open buffers",
-vim.keymap.set("n", "\\\\", function()
+k.set("n", "\\\\", function()
   builtin.buffers()
 end)
 
 -- "Resume the previous telescope picker",
-vim.keymap.set("n", ";;", function()
+k.set("n", ";;", function()
   builtin.resume()
 end)
 
 -- "Lists Diagnostics for all open buffers or a specific buffer",
-vim.keymap.set("n", "fd", function()
+k.set("n", "fd", function()
   builtin.diagnostics()
 end)
 
 -- "Lists Function names, variables, from Treesitter",
-vim.keymap.set("n", "fs", function()
+k.set("n", "fs", function()
   builtin.treesitter()
 end)
 
 -- "Lists all lsp symbols"
-vim.keymap.set("n", "ss", function()
+k.set("n", "ss", function()
   builtin.lsp_document_symbols()
 end)
 
 -- "Lists harpoon files"
-vim.keymap.set("n", "hl", "<cmd>Telescope harpoon marks<cr>")
+k.set("n", "hl", "<cmd>Telescope harpoon marks<cr>")
 
 -- "JsonFly"
-vim.keymap.set("n", "fj", "<cmd>Telescope jsonfly<cr>")
+k.set("n", "fj", "<cmd>Telescope jsonfly<cr>")
 
 -- "Open File Browser with the path of the current buffer",
-vim.keymap.set("n", "-", function()
+k.set("n", "-", function()
   local function telescope_buffer_dir()
     return vim.fn.expand("%:p:h")
   end
@@ -171,16 +167,16 @@ local mark = require("harpoon.mark")
 local term = require("harpoon.term")
 
 -- Mark a file
-vim.keymap.set("n", "<leader>ha", function()
+k.set("n", "<leader>ha", function()
   mark.add_file()
 end)
 
 -- Mark a terminal
-vim.keymap.set("n", "<leader>h1", function()
+k.set("n", "<leader>h1", function()
   term.gotoTerminal(1)
 end)
 
-vim.keymap.set("n", "<leader>h2", function()
+k.set("n", "<leader>h2", function()
   term.gotoTerminal(1)
 end)
 
@@ -189,31 +185,31 @@ local mc = require("multicursor-nvim")
 
 mc.setup()
 -- Add cursors above/below the main cursor.
-vim.keymap.set({ "n", "v" }, "<up>", function()
+k.set({ "n", "v" }, "<up>", function()
   mc.addCursor("k")
 end)
-vim.keymap.set({ "n", "v" }, "<down>", function()
+k.set({ "n", "v" }, "<down>", function()
   mc.addCursor("j")
 end)
 -- Add a cursor and jump to the next word under cursor.
-vim.keymap.set({ "n", "v" }, "<c-n>", function()
+k.set({ "n", "v" }, "<c-n>", function()
   mc.addCursor("*")
 end)
 -- Jump to the next word under cursor but do not add a cursor.
-vim.keymap.set({ "n", "v" }, "<c-s>", function()
+k.set({ "n", "v" }, "<c-s>", function()
   mc.skipCursor("*")
 end)
 -- Rotate the main cursor.
-vim.keymap.set({ "n", "v" }, "<left>", mc.nextCursor)
-vim.keymap.set({ "n", "v" }, "<right>", mc.prevCursor)
+k.set({ "n", "v" }, "<left>", mc.nextCursor)
+k.set({ "n", "v" }, "<right>", mc.prevCursor)
 
 -- Delete the main cursor.
-vim.keymap.set({ "n", "v" }, "<leader>x", mc.deleteCursor)
+k.set({ "n", "v" }, "<leader>x", mc.deleteCursor)
 
 -- Add and remove cursors with control + left click.
-vim.keymap.set("n", "<c-leftmouse>", mc.handleMouse)
+k.set("n", "<c-leftmouse>", mc.handleMouse)
 
-vim.keymap.set({ "n", "v" }, "<c-q>", function()
+k.set({ "n", "v" }, "<c-q>", function()
   if mc.cursorsEnabled() then
     -- Stop other cursors from moving.
     -- This allows you to reposition the main cursor.
@@ -223,7 +219,7 @@ vim.keymap.set({ "n", "v" }, "<c-q>", function()
   end
 end)
 
-vim.keymap.set("n", "<esc>", function()
+k.set("n", "<esc>", function()
   if not mc.cursorsEnabled() then
     mc.enableCursors()
   elseif mc.hasCursors() then
@@ -234,22 +230,22 @@ vim.keymap.set("n", "<esc>", function()
 end)
 
 -- Align cursor columns.
-vim.keymap.set("n", "<leader>a", mc.alignCursors)
+k.set("n", "<leader>a", mc.alignCursors)
 
 -- Split visual selections by regex.
-vim.keymap.set("v", "S", mc.splitCursors)
+k.set("v", "S", mc.splitCursors)
 
 -- Append/insert for each line of visual selections.
-vim.keymap.set("v", "I", mc.insertVisual)
-vim.keymap.set("v", "A", mc.appendVisual)
+k.set("v", "I", mc.insertVisual)
+k.set("v", "A", mc.appendVisual)
 
 -- match new cursors within visual selections by regex.
-vim.keymap.set("v", "M", mc.matchCursors)
+k.set("v", "M", mc.matchCursors)
 
 -- Rotate visual selection contents.
-vim.keymap.set("v", "<leader>t", function()
+k.set("v", "<leader>t", function()
   mc.transposeCursors(1)
 end)
-vim.keymap.set("v", "<leader>T", function()
+k.set("v", "<leader>T", function()
   mc.transposeCursors(-1)
 end)
