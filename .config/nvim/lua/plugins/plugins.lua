92c1468845637354ce83982f13f311e8992fb3c5
oliverjhernandez <oliverjhernandez@gmail.com>
2025-05-20 12:33:28 -0400

    [chore]: comment

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -12,6 +12,7 @@ return {
   },
   "taybart/b64.nvim",
 
+  -- temp while lazyvim is updated
   { "mason-org/mason.nvim", version = "1.11.0" },
   { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
