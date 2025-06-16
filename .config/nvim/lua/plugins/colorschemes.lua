fc25eb0b5dd5a880cfa3d7422d5f1e6a6924c41b
Oliver Hernández <oliverjhernandez@gmail.com>
2025-06-16 11:04:26 -0400

    feat: set gruvbox config in nvim

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,5 +1,13 @@
 return {
   { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "sainnhe/gruvbox-material", priority = 1000, opts = {} },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
