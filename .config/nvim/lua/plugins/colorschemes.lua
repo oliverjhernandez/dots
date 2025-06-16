116657cdc27b009e0e944f3b8f54fd7678da4e65
Oliver Hernández <oliverjhernandez@gmail.com>
2025-06-16 10:48:42 -0400

    chore: small refactor for colorschemes

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,5 @@
+return {
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "sainnhe/gruvbox-material", priority = 1000, opts = {} },
+}
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,15 +1,4 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
   "taybart/b64.nvim",
 
   -- temp while lazyvim is updated
